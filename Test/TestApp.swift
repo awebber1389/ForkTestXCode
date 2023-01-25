81e05dc7f10642432c45757dd4cae2787c9b30ee
Austin <awebber1389@gmail.com>
2023-01-24 20:12:08 -0500

    Initial Commit

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Austin Webber on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
